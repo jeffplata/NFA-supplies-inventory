b59a91433a734e6b4fde591d607d2360275439da
jeffp <jeffplata@yahoo.com>
2021-04-12 07:09:14 +0800

    where clause

--- sqlwhereclause.pas
+++ sqlwhereclause.pas
@@ -5,7 +5,11 @@ unit SQLWhereClause;
 interface
 
 uses
-  Classes, SysUtils, RegExpr;
+  Classes, SysUtils, RegExpr, SQLDB;  
+
+const
+  WHERECLAUSE_RE = 'where\s+(.+?)(\s+group\s+by\s+|\s+order\s+by\s+|\s+$)';
+  SELECTCLAUSE_RE = '(select\s+.+?)(\s+where\s+|\s+group\s+by\s+|\s+order\s+by\s+|\s+$)';
 
 type
 
@@ -13,24 +17,47 @@ type
 
   TSQLWhereClause = class
 
+  private
+    FDataset: TSQLQuery;
+    FSelectClause: string;
+    FWhereClause: string;
   public
-    procedure ExtractWhere;
-
+    constructor Create(ADataset: TSQLQuery);
+    destructor Destroy; override;
+    function ExtractClause( ARegEx: string ): string;
+  published
+    property Dataset: TSQLQuery read FDataset write FDataset;
+    property WhereClause: string read FWhereClause;
+    property SelectClause: string read FSelectClause;
   end;
 
 implementation
 
 { TSQLWhereClause }
 
-procedure TSQLWhereClause.ExtractWhere(ASQL: string);
+constructor TSQLWhereClause.Create(ADataset: TSQLQuery);
+begin
+  FDataset := ADataset;
+  FWhereClause:= ExtractClause(WHERECLAUSE_RE);
+  FSelectClause:= ExtractClause(SELECTCLAUSE_RE);
+end;
+
+destructor TSQLWhereClause.Destroy;
+begin
+  FDataset := nil;
+end;
+
+function TSQLWhereClause.ExtractClause(ARegEx: string): string;
 var
   re: TRegExpr;
 begin
+  Result := '';
   re := TRegExpr.Create;
   try
-    re.Expression:= 'where[\s]+(.+)(\s+group\s+by\s+|\s+order\s+by\s+|)';
-    re.InputString:= ASQL;
-    re.sub;
+    re.Expression:= ARegEx;
+    re.InputString:= FDataset.SQL.Text;
+    re.Exec;
+    Result := re.Match[1];
   finally
     re.free;
   end;
